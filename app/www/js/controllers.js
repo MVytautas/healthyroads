700d3fb2734c72191e09203a78f58cd0695881ea
Artiom <artiomnkkm@gmail.com>
2016-06-04 17:15:49 +0300

    Services init

--- app/www/js/controllers.js
+++ app/www/js/controllers.js
@@ -1,8 +1,99 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ngCordova'])
 
-.controller('MapCtrl', function($scope) {})
+.controller('MapCtrl', function($scope) {
 
-.controller('ActivityCtrl', function($scope) {
-  // $scope.activity = 'activity';
 })
-.controller('SettingsCtrl', function($scope) {});
+
+.controller('ActivityCtrl', function($scope, $cordovaDeviceMotion) {
+  // watch Acceleration options
+  $scope.options = { 
+      frequency: 100, // Measure every 100ms
+      deviation : 25  // We'll use deviation to determine the shake event, best values in the range between 25 and 30
+  };
+   
+  // Current measurements
+  $scope.measurements = {
+      x : null,
+      y : null,
+      z : null,
+      timestamp : null
+  };
+   
+  // Previous measurements    
+  $scope.previousMeasurements = {
+      x : null,
+      y : null,
+      z : null,
+      timestamp : null
+  };
+
+    //Start Watching method
+  $scope.startWatching = function() {     
+   
+      // Device motion configuration
+      $scope.watch = $cordovaDeviceMotion.watchAcceleration($scope.options);
+   
+      // Device motion initilaization
+      $scope.watch.then(null, function(error) {
+          console.log('Error');
+      }, function(result) {
+   
+          // DATA FROM ACCELEROMETER 
+          $scope.measurements.x = result.x;
+          $scope.measurements.y = result.y;
+          $scope.measurements.z = result.z;
+          $scope.measurements.timestamp = result.timestamp;                 
+         // TODO send data to service
+
+          // Detecta shake  
+          $scope.detectShake(result);  
+   
+      });     
+  };
+  // Stop watching method
+  $scope.stopWatching = function() {  
+      $scope.watch.clearWatch();
+  };
+
+  // Detect shake method      
+  $scope.detectShake = function(result) { 
+   
+      //Object to hold measurement difference between current and old data
+      var measurementsChange = {};
+   
+      // Calculate measurement change only if we have two sets of data, current and old
+      if ($scope.previousMeasurements.x !== null) {
+          measurementsChange.x = Math.abs($scope.previousMeasurements.x, result.x);
+          measurementsChange.y = Math.abs($scope.previousMeasurements.y, result.y);
+          measurementsChange.z = Math.abs($scope.previousMeasurements.z, result.z);
+      }
+   
+      // If measurement change is bigger then predefined deviation
+      if (measurementsChange.x + measurementsChange.y + measurementsChange.z > $scope.options.deviation) {
+          $scope.stopWatching();  // Stop watching because it will start triggering like hell
+          console.log('Shake detected'); // shake detected
+          setTimeout($scope.startWatching(), 1000);  // Again start watching after 1 sex
+   
+          // Clean previous measurements after succesfull shake detection, so we can do it next time
+          $scope.previousMeasurements = { 
+              x: null, 
+              y: null, 
+              z: null
+          }               
+   
+      } else {
+          // On first measurements set it as the previous one
+          $scope.previousMeasurements = {
+              x: result.x,
+              y: result.y,
+              z: result.z
+          }
+      }           
+   
+  };
+
+})
+
+.controller('SettingsCtrl', function($scope) {
+
+});
--- app/www/js/services.js
+++ app/www/js/services.js
@@ -1,1 +1,35 @@
-angular.module('starter.services', [])});
+angular.module('starter.services', [])
+
+.factory('data_processor',['$rootScope', function($rootScope) {
+  var data = [];
+
+  // Event listener exaple
+  // $rootScope.$on('data_processor:data_in', function(evt) {      
+  // });
+  return {
+    add_data : function(dt) {
+      // TODO serilize data to JSON
+      data.append(dt);
+      $rootScope.broadcast('data_processor:data_in');
+    }, 
+    get_data : function() {
+      return data;
+    }
+  };
+}])
+
+.factory('settings', ['$rootScope', function($rootScope) {
+  // Frequency default 100
+  var frequency = 100;
+  return {
+    setFrequncy : function(frqncy) {
+      frequency = frqncy;
+      $rootScope.broadcast('settings:frequency_changed');
+    },
+    getFrequncy : function() {
+
+    }
+  };
+}]);
+
+
